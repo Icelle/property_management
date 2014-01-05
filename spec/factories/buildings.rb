688ac38d9e4326dba38e1267cbaad04284ae7c9b
Icelle <icelleku@gmail.com>
2014-01-05 10:35:02 -0500

    edit buildings factory

--- spec/factories/buildings.rb
+++ spec/factories/buildings.rb
@@ -7,5 +7,12 @@ FactoryGirl.define do
     state "Massachusetts"
     postal_code "02339"
     description "Single family home"
+
+    factory :building_with_owners do
+      after(:create) do |building|
+        building.owners << FactoryGirl.create(:owner)
+        # create_list(:owner, 2, buildings: [building])
+      end
+    end
   end
 end
