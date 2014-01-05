4e266a7f96f97f84c1825bbe87b36bc6bb12c024
Icelle <icelleku@gmail.com>
2014-01-04 21:25:41 -0500

    make building tests pass- write controller and view files

---
+++ app/controllers/buildings_controller.rb
@@ -0,0 +1,26 @@
+class BuildingsController < ApplicationController
+
+  def new
+    @building = Building.new
+  end
+
+  def show
+    @building = Building.find(params[:id])
+  end
+
+  def create
+    @building = Building.new(building_params)
+    respond_to do |format|
+      if @building.save
+        format.html{redirect_to new_building_path, notice: "Building was successfully recorded."}
+      else
+        format.html {render action: 'new'}
+      end
+    end
+  end
+
+  def building_params
+    params.require(:building).permit(:street, :city, :state, :postal_code, :description)
+  end
+
+end
