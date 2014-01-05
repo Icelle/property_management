3751431bedd4e6628c2c155158f368a671365456
Icelle <icelleku@gmail.com>
2014-01-04 22:35:56 -0500

    edit validation to make test pass for email

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,7 @@
 class Owner < ActiveRecord::Base
   validates_presence_of :first_name
   validates_presence_of :last_name
-  validates_presence_of :email, email:true
+  validates :email, presence:true, email:true
 
   has_many :buildings
 end
